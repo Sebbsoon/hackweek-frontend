cc4c6eb77e8f96aae66eb7ea3feaa98e839b4ab3
Sebastian Thunberg <sebastian.thunberg@appliedtechnology.se>
2026-04-24 15:51:52 +0200

    global types

---
+++ src/types/global.d.ts
@@ -0,0 +1,24 @@
+declare global {
+  type UserGallery = {
+    id: string;
+    user_id: string;
+    title: string;
+    display_image_url?: string;
+    images?: Image[];
+    description?: string;
+  };
+  type Image = {
+    id: string;
+    gallery_id: string;
+    url: string;
+    title?: string;
+  };
+  type User = {
+    id: string;
+    username: string;
+    galleries: UserGallery[];
+    profile_picture_url: string;
+    description: string;
+  };
+}
+export {};
